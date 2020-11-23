d676626ee9d866d851f7762f456897e8b6ed7e2c
Bruno Santos <brunosrdeoliveira@gmail.com>
2020-11-23 15:07:45 -0300

    atualização da classe

--- Desafio-POO/src/main/empregado.java
+++ Desafio-POO/src/main/empregado.java
@@ -1,5 +1,42 @@
 package main;
 
-public class empregado {
-
+public class empregado extends Pessoa{
+	public int codigoSetor;
+	public double salarioBase;
+	public int imposto;
+	
+	
+	
+	public int getCodigoSetor() {
+		return codigoSetor;
+	}
+	public void setCodigoSetor(int codigoSetor) {
+		this.codigoSetor = codigoSetor;
+	}
+	public double getSalarioBase() {
+		return salarioBase;
+	}
+	public void setSalarioBase(double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+	public int getImposto() {
+		return imposto;
+	}
+	public void setImposto(int imposto) {
+		this.imposto = imposto;
+	}
+	
+	public empregado(String nome, String endereço, String telefone, String rg, String dataNacimento, int codigoSetor,
+			double salarioBase, int imposto) {
+		super(nome, endereço, telefone, rg, dataNacimento);
+		this.codigoSetor = codigoSetor;
+		this.salarioBase = salarioBase;
+		this.imposto = imposto;
+	}
+	
+	public double getCalcularSalario() {
+		double salario = ((salarioBase*(imposto/100))-salarioBase);
+		return salario;
+	}
+	
 }
