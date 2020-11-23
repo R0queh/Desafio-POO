8e967a88d30b5a83d5f8f8bc67e7563919af0da9
Joca <jocajuni@gmail.com>
2020-11-23 14:44:37 -0300

    update modificações

--- Desafio-POO/src/main/Operario.java
+++ Desafio-POO/src/main/Operario.java
@@ -2,19 +2,9 @@ package main;
 
 public class Operario extends Pessoa {
 	
-	public Operario(String nome, String endereco, String telefone, String rg, String dataNacimento) {
-		super(nome, endereco, telefone, rg, dataNacimento);
-		// TODO Auto-generated constructor stub
-	}
 	private int valorProducao;
 	private int comissao;
 		
-	public Operario(String nome, String endereco, String telefone, String rg, String dataNacimento, int valorProducao,
-			int comissao) {
-		super(nome, endereco, telefone, rg, dataNacimento);
-		this.valorProducao = valorProducao;
-		this.comissao = comissao;
-	}
 	public int getValorProducao() {
 		return valorProducao;
 	}
@@ -27,8 +17,11 @@ public class Operario extends Pessoa {
 	public void setComissao(int comissao) {
 		this.comissao = comissao;
 	}
-	
-	
-	
+	public Operario(String nome, String endereco, String telefone, String rg, String dataNacimento, int valorProducao,
+			int comissao) {
+		super(nome, endereco, telefone, rg, dataNacimento);
+		this.valorProducao = valorProducao;
+		this.comissao = comissao;
+	}	
 
 }
--- Desafio-POO/src/main/Vendedor.java
+++ Desafio-POO/src/main/Vendedor.java
@@ -1,5 +1,30 @@
 package main;
 
-public class Vendedor {
+public class Vendedor extends Pessoa {
+	private int valorVendas;
+	private int comissao;
+		
+	public Vendedor(String nome, String endereco, String telefone, String rg, String dataNacimento, int valorVendas,
+			int comissao) {
+		super(nome, endereco, telefone, rg, dataNacimento);
+		this.valorVendas = valorVendas;
+		this.comissao = comissao;
+	}
+	public int getValorVendas() {
+		return valorVendas;
+	}
+	public void setValorVendas(int valorVendas) {
+		this.valorVendas = valorVendas;
+	}
+	public int getComissao() {
+		return comissao;
+	}
+	public void setComissao(int comissao) {
+		this.comissao = comissao;
+	}
+	
+	
+	
+	}
+	
 
-}
